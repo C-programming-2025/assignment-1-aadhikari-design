9fc12eca9e5b904aa3e8a4ccedba3e14f11ad0a4
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:11:03 +0545

    Update q12.c

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,11 @@
-(a > b && c != 0)
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter a, b, c: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a > b && c != 0)
+        printf("Condition true\n");
+    else
+        printf("Condition false\n");
+    return 0;
+}
