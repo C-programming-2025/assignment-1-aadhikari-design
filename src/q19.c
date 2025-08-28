bffbe257ca1d076f8d3c0cd369480eb9d42f2d86
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:14:13 +0545

    Update q19.c

--- src/q19.c
+++ src/q19.c
@@ -1,1 +1,11 @@
-// Given three variables a, b, and c, write an expression that checks if a is equal to b and b is not equal to c.
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter a, b, c: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == b && b != c)
+        printf("Condition true\n");
+    else
+        printf("Condition false\n");
+    return 0;
+}
