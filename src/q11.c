8c2f6c94e45ca8f2b72cca2151abf53940e2a34d
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:10:47 +0545

    Update q11.c

--- src/q11.c
+++ src/q11.c
@@ -1,1 +1,9 @@
-x = (x + 1) * 3 - 10;
+#include <stdio.h>
+int main() {
+    int x;
+    printf("Enter x: ");
+    scanf("%d", &x);
+    x = (x + 1) * 3 - 10;
+    printf("Result = %d\n", x);
+    return 0;
+}
