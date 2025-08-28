bd2ad7a2db9279eb879c2f21f804d2c132f8800d
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:15:06 +0545

    Update q22.c

--- src/q22.c
+++ src/q22.c
@@ -1,1 +1,9 @@
-// Write a C expression that calculates the square root of the sum of two numbers, rounded to the nearest integer.
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Result = %d\n", (int)round(sqrt(a + b)));
+    return 0;
+}
