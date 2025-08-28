c054d51bab8237918affb6c849bf4cbd92609757
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:12:34 +0545

    Update q18.c

--- src/q18.c
+++ src/q18.c
@@ -1,1 +1,8 @@
-// Write a C expression that calculates the sum of the squares of three different numbers.
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("Sum of squares = %d\n", a*a + b*b + c*c);
+    return 0;
+}
