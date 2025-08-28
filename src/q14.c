3974b069b716f2aabe7a98deb3ded1f84704581c
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:11:29 +0545

    Update q14.c

--- src/q14.c
+++ src/q14.c
@@ -1,3 +1,11 @@
-x = x + y; 
-y = x - y; 
-x = x - y;
+#include <stdio.h>
+int main() {
+    int x,y;
+    printf("Enter x and y: ");
+    scanf("%d %d",&x,&y);
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    printf("After swap: x=%d, y=%d\n", x,y);
+    return 0;
+}
