278d36b444f75fefcc0fe264538d3a990d9df423
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:11:17 +0545

    Update q13.c

--- src/q13.c
+++ src/q13.c
@@ -1,1 +1,11 @@
-(num / 2 * 2 == num && num / 3 * 3 == num)
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num/2*2==num && num/3*3==num)
+        printf("Divisible by both 2 and 3\n");
+    else
+        printf("Not divisible by both 2 and 3\n");
+    return 0;
+}
