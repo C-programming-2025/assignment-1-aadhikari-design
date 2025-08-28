6d15aa802ed2d1ffe7dff6e0d2e86b43c4ffc774
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:14:27 +0545

    Update q20.c

--- src/q20.c
+++ src/q20.c
@@ -1,1 +1,11 @@
-// Write an expression that checks if a number is a multiple of either 3 or 5.
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 3 == 0 || num % 5 == 0)
+        printf("Multiple of 3 or 5\n");
+    else
+        printf("Not multiple of 3 or 5\n");
+    return 0;
+}
