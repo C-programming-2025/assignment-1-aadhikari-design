142aff8453b2ea584ebddbb92fc9abe78b0c8971
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:15:20 +0545

    Update q23.c

--- src/q23.c
+++ src/q23.c
@@ -1,1 +1,11 @@
-// Given a variable num, write an expression that checks if it is a power of 2.
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > 0 && (num & (num - 1)) == 0)
+        printf("Power of 2\n");
+    else
+        printf("Not power of 2\n");
+    return 0;
+}
