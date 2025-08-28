b26f0b53bddd0765430da54bc3367aaa319c3723
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:11:40 +0545

    Update q15.c

--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,11 @@
-(num > 0 && num % 2 == 0)
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num>0 && num%2==0)
+        printf("Positive and even\n");
+    else
+        printf("Not positive even\n");
+    return 0;
+}
