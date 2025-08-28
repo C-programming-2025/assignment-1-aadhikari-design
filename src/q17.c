2ce3f9f659bb5eb9732e61a5d3d8579a72900c1f
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:12:15 +0545

    Update q17.c

--- src/q17.c
+++ src/q17.c
@@ -1,1 +1,11 @@
-(ch >= 'B' && ch <= 'Y')
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    if(ch>='A' && ch<='Z')
+        printf("Uppercase\n");
+    else
+        printf("Not uppercase\n");
+    return 0;
+}
