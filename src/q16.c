452adfd270b0596249e0dd007d5a094dd775a459
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:11:52 +0545

    Update q16.c

--- src/q16.c
+++ src/q16.c
@@ -1,1 +1,8 @@
-(x + y) / 2.0
+#include <stdio.h>
+int main() {
+    int x,y;
+    printf("Enter x and y: ");
+    scanf("%d %d",&x,&y);
+    printf("Average = %.2f\n", (x+y)/2.0);
+    return 0;
+}
