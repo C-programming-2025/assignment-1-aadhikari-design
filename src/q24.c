01e75b23130eb72319ae1c559cdf66b739164d3e
aadhikari-design <aadhikari@maharishi.org.np>
2025-08-28 12:15:35 +0545

    Update q24.c

--- src/q24.c
+++ src/q24.c
@@ -1,1 +1,12 @@
-// Create an expression that checks if a given number is a perfect square.
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (sqrt(num) == (int)sqrt(num))
+        printf("Perfect square\n");
+    else
+        printf("Not perfect square\n");
+    return 0;
+}
